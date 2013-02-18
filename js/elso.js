1d7b3cce7aabfed90fc2e2a0f82fc62d6c316667
Antal Janos <dr4ko.draki@gmail.com>
2013-02-18 22:41:40 +0100

    masodik hazi

--- js/elso.js
+++ js/elso.js
@@ -1,10 +1,16 @@
-function nesze()
-{
-var szoveg = document.getElementById('valami').value;
-document.getElementById('valami2').value = szoveg;
-}
-
-function hossz()
-{
-alert('A szoveg hossza a dobozokban: '+ document.getElementById('valami2').value.length);
-}
+var gomb, valami, valami2;
+	function onClick()  {
+		alert('A szoveg hossza a dobozokban: ' + valami2.value.length);
+	}
+	 function onKeyUp() {
+		valami2.value = valami.value;
+	}
+	function setUp() {
+		gomb = document.getElementById('gomb');
+		valami = document.getElementById('valami');
+		valami2 = document.getElementById('valami2');
+		valami.addEventListener('keyup', onKeyUp);
+		gomb.addEventListener('click', onClick);
+	}
+	
+document.addEventListener('DOMContentLoaded', setUp);
